5b24ff53730358af75d9dd76445d9cd739c7e220
Li Lin <guru.lin@gmail.com>
2019-10-09 23:22:03 +0800

    skeleton

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Container from "@material-ui/core/Container";
+import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
+import Link from "@material-ui/core/Link";
+import Dashboard from "./Dashboard";
+
+export default function App() {
+  return (
+    <Container>
+      <Dashboard />
+    </Container>
+  );
+}
