bc81ad2df0cc5a1b64edb37320b6513177ebffc5
Li Lin <guru.lin@gmail.com>
2019-10-10 00:33:47 +0800

    clean up unused import

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React from "react";
 import Container from "@material-ui/core/Container";
-import Typography from "@material-ui/core/Typography";
-import Box from "@material-ui/core/Box";
-import Link from "@material-ui/core/Link";
 import Dashboard from "./Dashboard";
 
 export default function App() {
