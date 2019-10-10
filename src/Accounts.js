3900caa00960aa41efc2c390dbfc1c4e71a2d530
Li Lin <guru.lin@gmail.com>
2019-10-10 12:54:01 +0800

    tesed deployment

--- src/Accounts.js
+++ src/Accounts.js
@@ -8,6 +8,7 @@ import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
+import CircularProgress from "@material-ui/core/CircularProgress";
 
 import Title from "./Title";
 import { Button } from "@material-ui/core";
@@ -33,38 +34,51 @@ export default class Accounts extends Component {
   }
 
   fetchTopAccounts = async () => {
-    const response = await fetch("/api/top/10");
+    const response = await fetch("/api/top/18");
     const payload = await response.json();
     console.log(payload);
 
-    let newRows = [];
-    for (var val of payload.accounts) {
-      let bal = val.balances[0];
-      let account = createData(
-        val.account_id,
-        val.account_id,
-        val.nickname,
-        bal.amount,
-        val.currency,
-        bal.last_updated
-      );
-      newRows.push(account);
-    }
+    if (payload.hasOwnProperty("accounts")) {
+      let newRows = [];
+      for (var val of payload.accounts) {
+        let bal = val.balances[0];
+        let account = createData(
+          val.account_id,
+          val.account_id,
+          val.nickname,
+          bal.amount,
+          val.currency,
+          bal.last_updated
+        );
+        newRows.push(account);
+      }
 
-    this.setState({ rows: newRows });
+      this.setState({ rows: newRows });
+    }
   };
+
+  componentDidMount() {
+    this.interval = setInterval(() => this.fetchTopAccounts(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
 
   render() {
     let rows = this.state.rows;
     return (
       <Container>
         <Title>Recent Updated Accounts</Title>
+        <div>
+          <CircularProgress />
+        </div>
         <Table size="small">
           <TableHead>
             <TableRow>
               <TableCell>Account Id</TableCell>
               <TableCell>Account Name</TableCell>
-              <TableCell>Balance</TableCell>
+              <TableCell align="right">Balance</TableCell>
               <TableCell>Currency</TableCell>
               <TableCell align="right">Last Updated</TableCell>
             </TableRow>
@@ -74,18 +88,13 @@ export default class Accounts extends Component {
               <TableRow key={row.id} selected={row.accountId === "5201442870"}>
                 <TableCell>{row.accountId}</TableCell>
                 <TableCell>{row.accountName}</TableCell>
-                <TableCell>{row.balance}</TableCell>
+                <TableCell align="right">{row.balance}</TableCell>
                 <TableCell>{row.currency}</TableCell>
                 <TableCell align="right">{row.lastUpdated}</TableCell>
               </TableRow>
             ))}
           </TableBody>
         </Table>
-        <div>
-          <Button color="primary" onClick={this.fetchTopAccounts}>
-            Fetch
-          </Button>
-        </div>
       </Container>
     );
   }
